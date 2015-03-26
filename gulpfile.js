8d36f844c275083168599b809e37cdbf47e5d173
david <david@accend.io>
2015-03-25 19:19:12 -0500

    Generated files after adding ios platform

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -47,4 +49,11 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('./www/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
